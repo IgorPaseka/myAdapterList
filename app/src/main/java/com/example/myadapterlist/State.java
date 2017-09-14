9bcec60513949b7e2880c0abc0edfb9d5fda9a6c
Пасека И.Н <igorp@gc.kimes.dnepr.energy.gov.ua>
2017-09-14 16:32:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myadapterlist/State.java
@@ -0,0 +1,40 @@
+package com.example.myadapterlist;
+
+
+public class State {
+    private String name;
+    private String capital;
+    private int flagResource;
+
+    public State(String name, String capital, int flag){
+
+        this.name = name;
+        this.capital = capital;
+        this.flagResource = flag;
+
+    }
+
+    public String getName() {
+        return this.name;
+
+    }
+
+    public String getCapital(){
+        return this.capital;
+
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+
+    }
+
+    public int getFlagResource(){
+        return this.flagResource;
+    }
+
+    public void setFlagResource(int flagResource){
+        this.flagResource = flagResource;
+
+    }
+}
